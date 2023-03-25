4de5ab75a6403396536471da09e13e28cdc6e21c
Fred Banda <fredcbanda@hotmail.com>
2023-03-26 00:26:08 +0200

    installed Bootstrap 5

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+require jquery3
+require popper
+require bootstrap-sprockets
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
+
